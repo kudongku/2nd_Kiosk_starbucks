3f71a9c87fd0dc89257d88d9bfafe026b1c88420
kudongku <kudongku@naver.com>
2024-01-04 20:27:50 +0900

    ad

---
+++ Drink_type.java
@@ -0,0 +1,9 @@
+public class Drink_type {
+    public String name;
+    public String explanation;
+
+    public Drink_type(String name, String explanation) {
+        this.name = name;
+        this.explanation = explanation;
+    }
+}
